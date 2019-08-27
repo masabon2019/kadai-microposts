85be930effd4677e8c8830d3c6aea7e196df24e8
masafumi.tokuhisa <tokuhisa.st@gmail.com>
2019-08-27 06:37:55 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -14,9 +16,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     
+    #saveとif文判定を同時に行っている
     if @user.save
       flash[:success] = 'ユーザーを登録しました。'
       redirect_to @user
+      #render_to ("users/#{@user.id}")
     else
       flash.now[:danger] = 'ユーザーの登録に失敗しました。'
       render :new
