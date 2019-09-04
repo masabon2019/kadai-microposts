006f6a6adcdfada9b7b334073e01af2a2aeeb953
masafumi.tokuhisa <tokuhisa.st@gmail.com>
2019-09-04 04:54:11 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -5,7 +5,7 @@ class ToppagesController < ApplicationController
       @micropost = current_user.microposts.build 
       
       #一覧用に取得、インスタンスに代入
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
     end
   end
 end
